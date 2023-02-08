6dba93e2faa1ec6429a33e9c30ee3561c2760ad2
Augustinas Malinauskas <augustinasmal@gmail.com>
2023-02-08 16:06:51 +0000

    init

---
+++ solution.go
@@ -0,0 +1,11 @@
+package main
+
+import "errors"
+
+var DuplicateEventError = errors.New("duplicate event")
+
+// deduplicateEvent - checks if the event was processed before.
+// Solution should be persisted under application restarts.
+func deduplicateEvent(event *Event) (*Event, error) {
+	panic("not implemented")
+}
